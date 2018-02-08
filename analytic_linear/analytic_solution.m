c85c25f41553a5b29ea576daaf1d4d1431c9ccb0
ben <bc14714@my.bristol.ac.uk>
2018-02-08 11:06:37 +0000

    reorganised, analytic solution is working!

--- analytic_linear/analytic_solution.m
+++ analytic_linear/analytic_solution.m
@@ -6,6 +6,14 @@
 
 %TODO: include impact
 %TODO: Check Forcing terms..
+%TODO: Check the sign(rocking) terms,,,
+
+ss = -P/A;
+%Initial Conditions.
+phi_0  = ss - 0.5*ss;
+dphi_0 = 0;
+psi_0  = 0;
+dpsi_0 = 0;
 
 parameters
 
@@ -16,10 +24,9 @@ F = params(7); Q = params(8);
 beta = params(9); omega = params(10);  
 
 %Left rocking +  or right rocking -?
-rocking = -1;
+rocking = sign(phi_0);
     
 %Eigenvalues x4 2 complex, 2 real.
-
 lambda_test = (1/sqrt(2))*sqrt((E + A) + sqrt((E+A)^2 - 4*(A*E - B*D)));
 discrim = sqrt((E+A)^2 - 4*(A*E - B*D));
 
@@ -28,39 +35,20 @@ lambda_2 = -lambda_1;
 lambda_3 =(1/sqrt(2))*sqrt((E+A) - discrim);
 lambda_4 = - lambda_3;
 
-%Big A Matrix
-LinearMatrix = [0, 1, 0, 0;...
-                A, 0, B, 0;...
-                0, 0, 0, 1;...
-                D, 0, E, 0];
-
-[evec, ~] = eig(LinearMatrix);
-
 %Eigenvectors these are correct now! missing a minus sign.
 v_1 = [-B/(A - lambda_1^2);-B*lambda_1/(A - lambda_1^2); 1; lambda_1];
 v_2 = [-B/(A - lambda_2^2);-B*lambda_2/(A - lambda_2^2); 1; lambda_2];
 v_3 = [-B/(A - lambda_3^2);-B*lambda_3/(A - lambda_3^2); 1; lambda_3];
 v_4 = [-B/(A - lambda_4^2);-B*lambda_4/(A - lambda_4^2); 1; lambda_4];
 
-%Initial Conditions.
-phi_0  = sign(rocking)*P/A;
-dphi_0 = 0;
-psi_0  = 0.01;
-dpsi_0 = 0;
-
-%Correct up to here. Initial conditions are correct.
-
 %Initial forcing term:
 eigPoly = B*D -(E - omega^2)*(A + omega^2);
 R_phi = beta*omega^2*(F*B - C*(E - omega^2))/eigPoly;
 R_psi = beta*omega^2*((F*B - C*(E - omega^2)*(A+omega^2))/(B*eigPoly)  - (C/B));
 
-
-
 %Transformed Initial Conditions
-phi_bar = phi_0 - sign(rocking)*P/A - R_phi;
+phi_bar = phi_0  + sign(rocking)*P/A - R_phi;
 psi_bar = psi_0 - R_psi;
-
 
 %Integration pre-constants:
 A_1 = v_1(1); A_2 = v_3(1);
@@ -80,7 +68,7 @@ c_2 = (1/2)*((-dphi_0/A_3 + phi_bar/A_1) - (A_2/A_1)*A_12*(psi_bar - phi_bar/A_1
 c_1 = (1/2)*((dphi_0/A_3 + phi_bar/A_1) - (A_2/A_1)*A_12*(psi_bar - phi_bar/A_1)...
               - (A_4/A_3)*A_56*(dpsi_0/A_5 - dphi_0/A_3));
 
-t = linspace(0,4,400);
+t = linspace(0,2,400);
 y = c_1*v_1*exp(lambda_1*t) + c_2*v_2*exp(lambda_2*t) + c_3*v_3*exp(lambda_3*t) ...
     + c_4*v_4*exp(lambda_4*t) + [R_phi; 0; R_psi;0]*cos(omega*t) + sign(rocking)*[P/A;0;0;0];
 
--- analytic_linear/analytic_solver.m
+++ analytic_linear/analytic_solver.m
@@ -1,68 +1,97 @@
 %%Analytic_composite
 
-%TODO: Want to find the first zero.....
 %TODO: want to define a function that gives me position of block...
 %TODO: Dodgy things happen can happen, system seems to gain energy...
-%TODO: unless problem with linearising...       
+%TODO: unless problem with linearising...
+%TODO: Can break with r ~ 0.5 thanks to v. small steps, but i think thats
+%okay.
+
 clear all
 
 parameters
 
 tLim = 10;
-IC = [P/A ,0, 0,0.01]
-crossTime = getCrossTime(IC,tLim);
+ss = -params(4)/params(1);
+IC = [ss-0.001*ss,0,0,0];
 yTotal = [];
 tTotal = [];
 currentTime = 0;
+r =0.5;
 
-
-%NOT WORKING. I think it is because solution doesnt know which side to
-%start on as im starting at phi = 0.
 for i = 1:20
+    
+    %Find the crossing time, given "initial conditions"
     crossTime = getCrossTime(IC,tLim);
+    
+    %Fixes case where block does not overturn
+    if (crossTime == -1) 
+        tVec = linspace(0,tLim,200);
+        y = analytic(IC,tVec);
+        yTotal = [yTotal,y];
+        tTotal = [tTotal,tVec + currentTime];
+        break
+    end
+        
+    
+    %Solve the solution up to the crossing time.
     tVec = linspace(0,crossTime,200);
     y = analytic(IC,tVec);
+    
+    %Initial conditions for next impact motion are end of the previous
     IC = y(:,end)';
-    IC(1) = IC(1) + sign(IC(2))*eps;
+    IC(2) = r*IC(2);
+    
+    %either introduce eps as error or set equal to zero and pass rocking sign as extra
+    %parameter.
+    IC(1) = sign(IC(2))*eps;
+    
+    %Add solution to total solution.
     yTotal = [yTotal,y];
     tTotal = [tTotal,tVec + currentTime];
     
-    %To get times to match up
+    %To match offest in times
     currentTime = currentTime + crossTime;
-    IC;
-    i;
 end
-size(yTotal)
-size(tTotal)
 
 plot(tTotal,yTotal);
 legend('phi','dphi','psi','dpsi');
+title('Analytic Solution');
+grid on
     
     
-    
-
-
-
-
 function crossTime =  getCrossTime(IC,tLim)
+
+  %Initial rocking sign
   initSign = sign(IC(1));
-  tVec = linspace(0,tLim,100);
+  tVec = linspace(0,tLim,1000);
   
   %Solution up to time limit
   y = analytic(IC,tVec);
   
+  %Which side is the block on?
   signVec = sign(y(1,:));
-  x0 = tVec(find(signVec == -initSign, 1 ));
   
+  %Find rough time of impact, need the minus one? i think fzero searches
+  %forwards.
+  timeImpact = tVec(find(signVec == -initSign, 1 ));
+  
+  %If no impact, do special things
+  if(isempty(timeImpact))
+      fprintf('Block does not impact in %ds interval\n',tLim);
+      crossTime = -1;
+      return
+  end
+    
   %Gives error if escapes bounds. want to catch.
-  crossTime = fzero(@(t)phiAngle(IC,t),1);
+  options = optimset('Tolx',eps);
+  crossTime = fzero(@(t)phiAngle(IC,t),timeImpact,options);
+  
 
 end
 
 function phiAng = phiAngle(IC,t)
     y = analytic(IC,t);
     phiAng = y(1,:);
-    
 end
 
 function y = analytic(IC,t)
@@ -73,14 +102,21 @@ A = params(1); B = params(2);
 C = params(3); P = params(4);
 D = params(5); E = params(6);
 F = params(7); Q = params(8);
-beta = params(9); omega = params(10);  
+beta = params(9); omega = params(10); 
 
-%Left rocking +  or right rocking -?
+%Steady State for rocking around left corner.
+ss = -P/A;
+
+%Initial Conditions.
+phi_0  = IC(1);
+dphi_0 = IC(2);
+psi_0  = IC(3);
+dpsi_0 = IC(4);
+
+%Left rocking +  or right rocking?
 rocking = sign(IC(1));
     
 %Eigenvalues x4 2 complex, 2 real.
-
-lambda_test = (1/sqrt(2))*sqrt((E + A) + sqrt((E+A)^2 - 4*(A*E - B*D)));
 discrim = sqrt((E+A)^2 - 4*(A*E - B*D));
 
 lambda_1 = (1/sqrt(2))*sqrt((E+A) + discrim);
@@ -88,45 +124,20 @@ lambda_2 = -lambda_1;
 lambda_3 =(1/sqrt(2))*sqrt((E+A) - discrim);
 lambda_4 = - lambda_3;
 
-%Big A Matrix
-LinearMatrix = [0, 1, 0, 0;...
-                A, 0, B, 0;...
-                0, 0, 0, 1;...
-                D, 0, E, 0];
-
-[evec, ~] = eig(LinearMatrix);
-
 %Eigenvectors these are correct now! missing a minus sign.
 v_1 = [-B/(A - lambda_1^2);-B*lambda_1/(A - lambda_1^2); 1; lambda_1];
 v_2 = [-B/(A - lambda_2^2);-B*lambda_2/(A - lambda_2^2); 1; lambda_2];
 v_3 = [-B/(A - lambda_3^2);-B*lambda_3/(A - lambda_3^2); 1; lambda_3];
 v_4 = [-B/(A - lambda_4^2);-B*lambda_4/(A - lambda_4^2); 1; lambda_4];
 
-%Initial Conditions.
-
-% phi_0  = sign(rocking)*P/A;
-% dphi_0 = 0;
-% psi_0  = 0;
-% dpsi_0 = 0;
-
-phi_0  = IC(1);
-dphi_0 = IC(2);
-psi_0  = IC(3);
-dpsi_0 = IC(4);
-
-%Correct up to here. Initial conditions are correct.
-
 %Initial forcing term:
 eigPoly = B*D -(E - omega^2)*(A + omega^2);
 R_phi = beta*omega^2*(F*B - C*(E - omega^2))/eigPoly;
 R_psi = beta*omega^2*((F*B - C*(E - omega^2)*(A+omega^2))/(B*eigPoly)  - (C/B));
 
-
-
 %Transformed Initial Conditions
-phi_bar = phi_0 - sign(rocking)*P/A - R_phi;
+phi_bar = phi_0 - sign(rocking)*ss - R_phi;
 psi_bar = psi_0 - R_psi;
-
 
 %Integration pre-constants:
 A_1 = v_1(1); A_2 = v_3(1);
@@ -147,7 +158,7 @@ c_1 = (1/2)*((dphi_0/A_3 + phi_bar/A_1) - (A_2/A_1)*A_12*(psi_bar - phi_bar/A_1)
               - (A_4/A_3)*A_56*(dpsi_0/A_5 - dphi_0/A_3));
 
 y = c_1*v_1*exp(lambda_1*t) + c_2*v_2*exp(lambda_2*t) + c_3*v_3*exp(lambda_3*t) ...
-    + c_4*v_4*exp(lambda_4*t) + [R_phi; 0; R_psi;0]*cos(omega*t) + sign(rocking)*[P/A;0;0;0];
+    + c_4*v_4*exp(lambda_4*t) + [R_phi; 0; R_psi;0]*cos(omega*t) + sign(rocking)*[ss;0;0;0];
 
 end
 
